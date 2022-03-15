0685f49a5fa08f30088d76a047bb951aacc0a9d3
HAJINHEE <hajinheee@gmail.com>
2022-03-15 18:42:13 +0900

    Titanic

--- titanic/templates.py
+++ titanic/templates.py
@@ -4,9 +4,12 @@ from titanic import TitanicModel
 
 
 class TitanicTemplate(object):
-    def __init__(self):
+    def __init__(self, train_fname, test_fname):
         self.model = Model()
         self.dataset = Dataset()
-        self.titanic = TitanicModel()
+        self.titanic = TitanicModel(train_fname='train.csv', test_fname='test.csv')
+        self.train = self.model.new_model(train_fname)
+        self.test = self.model.new_model(test_fname)
+
 
 
